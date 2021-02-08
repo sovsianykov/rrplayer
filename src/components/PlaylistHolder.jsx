a1aecde9399788b9b9a4a6271dc8c385395bb76f
sovsianykov <swebproger@gmail.com>
2021-02-09 01:05:23 +0200

    css is redy 2

--- src/components/PlaylistHolder.jsx
+++ src/components/PlaylistHolder.jsx
@@ -6,7 +6,7 @@ function PlaylistHolder({ Playlist }) {
       <div className="playlist">
         {Playlist.map((song, i) => {
           return (
-            <div>
+            <div key={`${i + 100}`}>
               <span key={`${i}`}>{song.name}</span>
               <audio controls key={`${song}_${i}`}>
                 <source src={song.src} className="list" />
